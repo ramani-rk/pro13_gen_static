9668c276a804c55d6d349b14e62396506290bf9b
ramani-rk <ramanikanthsasi@gmail.com>
2023-11-22 20:21:36 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def dhoni(request):
+    return render (request,'dhoni.html')
+
+def raina(request):
+    return render (request,'raina.html')
+
+def jaddu(request):
+    return render (request,'jaddu.html')
